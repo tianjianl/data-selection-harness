44841e2af6e3436610c4fa21963d85684e0169bf
tianjianl <tli104@jhu.edu>
2024-04-19 21:38:20 +0000

    added scripts for downloading models

---
+++ downloads/download_llama3.py
@@ -0,0 +1,15 @@
+import transformers
+import torch
+import os
+
+#model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+#model_id = "meta-llama/Llama-2-7b-hf"
+model_id = "meta-llama/Meta-Llama-3-8B"
+
+pipeline = transformers.pipeline(
+  "text-generation",
+  model=model_id,
+  model_kwargs={"torch_dtype": torch.bfloat16},
+  device="cuda",
+  token=os.environ["HF_TOKEN"]
+)
---
+++ downloads/download_wizard_lm_filtered.py
@@ -0,0 +1,3 @@
+from datasets import load_dataset
+
+dataset = load_dataset("cognitivecomputations/WizardLM_evol_instruct_V2_196k_unfiltered_merged_split")
